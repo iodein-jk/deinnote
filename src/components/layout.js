54a3193ed980dff49c21c98793a64e4f54baf894
iodein-jk <ken.record1210@gmail.com>
2020-12-11 17:35:53 +0900

    Google Analytics 追加

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 import HeaderBlock from "../components/header"
 import FooterBlock from "../components/fotter"
 import Bio from "../components/bio"
